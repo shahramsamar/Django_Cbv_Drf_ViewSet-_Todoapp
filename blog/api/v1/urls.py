c1a7565c14a05dc4f140114d1231c08d65ea020b
shahramsamar <shahramsamar2010@gmail.com>
2024-12-22 22:52:25 +0330

    Update urls.py

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -2,24 +2,17 @@ from django.urls import path
 from blog.api.v1 import views
 
 
-from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
-router.register('post', views.PostModelViewSet,basename='post')
-app_name = 'api-v1'  
-
-urlpatterns = router.urls
 
 
 
-
-# urlpatterns = [
-#     path('post/', 
-#          views.PostViewSet.as_view({'get':'list','post':'create'}), name='post-list'),  
-#     path('post/<int:pk>/',
-#           views.PostViewSet.as_view({'get':'retrieve',
-#                                      'put':'update',
-#                                      'patch':'partial_update',
-#                                      'delete':'destroy'}), name='post-detail'),  
+urlpatterns = [
+    path('post/', 
+         views.PostModelViewSet.as_view({'get':'list','post':'create'}), name='post-list'),  
+    path('post/<int:pk>/',
+          views.PostModelViewSet.as_view({'get':'retrieve',
+                                     'put':'update',
+                                     'patch':'partial_update',
+                                     'delete':'destroy'}), name='post-detail'),  
                                 
-# ]
+]
 
