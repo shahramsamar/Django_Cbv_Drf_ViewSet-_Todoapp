ed388134a05cfde9bf36ebd5693779f601e17add
shahramsamar <shahramsamar2010@gmail.com>
2024-12-22 22:49:05 +0330

    Update urls.py

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from blog.api.v1 import views
-from rest_framework.routers import DefaultRouter
 
+
+from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('post', views.PostModelViewSet,basename='post')
 app_name = 'api-v1'  
